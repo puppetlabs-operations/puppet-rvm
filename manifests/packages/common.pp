9f56325e157f3320128379ddc58e1e6654a23759
Samuel <samuel@puppetlabs.com>
2019-05-13 12:47:11 -0500

    Update common.pp
    add new dev key as per https://github.com/rvm/rvm/issues/4573

--- manifests/packages/common.pp
+++ manifests/packages/common.pp
@@ -4,7 +4,7 @@ class rvm::packages::common {
   }
   
   exec { 'install-rvm':
-    command => "curl -sSL https://rvm.io/mpapis.asc | gpg --import - && curl -sSL https://get.rvm.io | bash -s stable",
+    command => "curl -sSL https://rvm.io/mpapis.asc | gpg --import - && gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB && curl -sSL https://get.rvm.io | bash -s stable",
     creates => '/usr/local/rvm/bin/rvm',
   }
   file { '/tmp/rvm':
